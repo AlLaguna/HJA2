47ffbace54b1792cc6cf2cd8d7c775b4348619d0
AlLaguna <79445367+AlLaguna@users.noreply.github.com>
2022-11-08 09:28:15 +0100

    Create Arreglos tipos parejas.java

---
+++ Arreglos tipos parejas.java
@@ -0,0 +1,11 @@
+//Aumentar tamaño array jugadas (+5)
+
+//Calcular 1era y 2nda carta mas alta del board | calcularCombos() -> 638
+
+
+//En los bucles de debajo
+//overpair (pareja	en	mano	mejor	que	la	carta	mas	alta	del	board)  PM > 1CA	
+//top pair (pareja	con	la	carta	mas	alta	del	board)  PM(1CA,X)
+//pocket	pair	below	top	pair (pareja	en	mano	con	 cartas	menores	 que	la	mas	alta	 del	 board	 pero	 que	 no	es	 débil) PM < 1CA
+//middle	pair (pareja con	 la	 segunda	 carta	 mas	 alta	 del	 board)	 PM(2CA,X)
+//weak	 pair (otras	 parejas) 
